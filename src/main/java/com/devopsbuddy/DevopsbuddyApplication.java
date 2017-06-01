5531e76e716cac6de09d2f507f637dc132cc09ae
Donald F. Coffin <donald.coffin@reminetworks.com>
2017-05-31 23:37:06 -0400

    Added password encryption to Spring Security

--- src/main/java/com/devopsbuddy/DevopsbuddyApplication.java
+++ src/main/java/com/devopsbuddy/DevopsbuddyApplication.java
@@ -10,6 +10,7 @@ import com.devopsbuddy.utils.UserUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -17,7 +18,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 @SpringBootApplication
-public class DevopsbuddyApplication {
+public class DevopsbuddyApplication implements CommandLineRunner {
 
 	/** The application logger **/
 	private static final Logger LOG = LoggerFactory.getLogger(DevopsbuddyApplication.class);
@@ -29,6 +30,7 @@ public class DevopsbuddyApplication {
 		SpringApplication.run(DevopsbuddyApplication.class, args);
 	}
 
+	@Override
 	public void run(String... args) throws Exception {
 		User user = UserUtils.createBasicUser();
 		Set<UserRole> userRoles = new HashSet<>();
--- src/main/java/com/devopsbuddy/config/SecurityConfig.java
+++ src/main/java/com/devopsbuddy/config/SecurityConfig.java
@@ -4,13 +4,16 @@ import com.devopsbuddy.backend.service.UserSecurityService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import java.security.SecureRandom;
 import java.util.Arrays;
 import java.util.List;
 
@@ -29,6 +32,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private Environment env;
+
+    /** The encryption SALT. **/
+    private static final String SALT = "fdalkjalk;3jlwf03sgh@(";
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(12, new SecureRandom(SALT.getBytes()));
+    }
 
     /** Public URLs. **/
     private static final String[] PUBLIC_MATCHERS = {
@@ -69,6 +80,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth
-                .userDetailsService(userSecurityService);
+                .userDetailsService(userSecurityService)
+                .passwordEncoder(passwordEncoder());
     }
 }
