b23405cbd53cbd0024a5fcbb5c6e9eae277adf6e
Donald F. Coffin <donald.coffin@reminetworks.com>
2017-06-01 17:49:48 -0400

    Created PasswordResetToken entity and repository and integration tests

--- src/main/java/com/devopsbuddy/backend/persistence/domain/backend/User.java
+++ src/main/java/com/devopsbuddy/backend/persistence/domain/backend/User.java
@@ -19,6 +19,9 @@ public class User implements Serializable, UserDetails {
     /** The Serial Version UID for Serializable classes. **/
     private static final long serialVersionUID = 1L;
 
+    /**
+     * Default constructor.
+     */
     public User() {
 
     }
@@ -62,6 +65,21 @@ public class User implements Serializable, UserDetails {
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private Set<UserRole> userRoles = new HashSet<>();
+
+    @OneToMany(
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY,
+            mappedBy = "user"
+    )
+    private Set<PasswordResetToken> passwordResetTokens = new HashSet<>();
+
+    public Set<PasswordResetToken> getPasswordResetTokens() {
+        return passwordResetTokens;
+    }
+
+    public void setPasswordResetTokens(Set<PasswordResetToken> passwordResetTokens) {
+        this.passwordResetTokens = passwordResetTokens;
+    }
 
     public long getId() {
         return id;
