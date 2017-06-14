43e04b318a2d91b68b4a8a74cc1a1755c3566e3b
Donald F. Coffin <donald.coffin@reminetworks.com>
2017-06-14 18:45:27 -0400

    Completed the S3 Service

--- src/main/java/com/devopsbuddy/config/ApplicationConfig.java
+++ src/main/java/com/devopsbuddy/config/ApplicationConfig.java
@@ -1,6 +1,16 @@
 package com.devopsbuddy.config;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -15,4 +25,18 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @PropertySource("file:///${user.home}/Git/dfcoffin/.devopsbuddy/application-common.properties")
 public class ApplicationConfig {
+
+    @Value("${aws.s3.profile}")
+    private String awsProfileName;
+
+    @Bean
+    public AmazonS3Client s3Client() {
+        AWSCredentials credentials = new ProfileCredentialsProvider(awsProfileName).getCredentials();
+        AmazonS3Client s3Client = new AmazonS3Client(credentials);
+        //  AmazonS3 s3Client = AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(credentials)).build();
+
+        Region region = Region.getRegion(Regions.US_WEST_2);
+        s3Client.setRegion(region);
+        return s3Client;
+    }
 }
